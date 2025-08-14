8169aae6ac85ba1dc971de03b40d768154923816
Razin Uddin <razinn70@gmail.com>
2025-08-14 07:54:22 +0000

    implement week 10

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDk1v5Xyp3gzulxqR5aBcOZehm4r-LGIUo",
+  authDomain: "cuisine-cloud-wfemi.firebaseapp.com",
+  projectId: "cuisine-cloud-wfemi",
+  storageBucket: "cuisine-cloud-wfemi.firebasestorage.app",
+  messagingSenderId: "65923384921",
+  appId: "1:65923384921:web:3b56799493d5743ff11348"
 };
 
 // Initialize Firebase
